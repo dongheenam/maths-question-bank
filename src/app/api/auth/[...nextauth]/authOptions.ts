3836f406751402f425890dd09ec8f1b584a847f5
Donghee Nam <57467269+dongheenam@users.noreply.github.com>
2023-04-14 17:36:37 +1000

    Source authOptions from a seperate file other than route.ts

---
+++ src/app/api/auth/[...nextauth]/authOptions.ts
@@ -0,0 +1,32 @@
+import type { NextAuthOptions } from 'next-auth';
+import AzureADProvider from 'next-auth/providers/azure-ad';
+import { MongoDBAdapter } from '@next-auth/mongodb-adapter';
+
+import client from '@/common/mongoClient';
+
+if (
+  process.env.AZURE_AD_CLIENT_ID === undefined ||
+  process.env.AZURE_AD_CLIENT_SECRET === undefined ||
+  process.env.NEXTAUTH_SECRET === undefined
+) {
+  throw new Error('environment variables for auth not loaded!');
+}
+
+const authOptions: NextAuthOptions = {
+  adapter: MongoDBAdapter(client.connect(), {
+    collections: {
+      Accounts: 'auth.accounts',
+      Sessions: 'auth.sessions',
+      Users: 'auth.users',
+      VerificationTokens: 'auth.verificationTokens',
+    },
+  }),
+  providers: [
+    AzureADProvider({
+      clientId: process.env.AZURE_AD_CLIENT_ID,
+      clientSecret: process.env.AZURE_AD_CLIENT_SECRET,
+      tenantId: process.env.AZURE_AD_TENANT_ID,
+    }),
+  ],
+};
+export default authOptions;
--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,34 +1,6 @@
-import NextAuth, { NextAuthOptions } from 'next-auth';
-import AzureADProvider from 'next-auth/providers/azure-ad';
-import { MongoDBAdapter } from '@next-auth/mongodb-adapter';
+import NextAuth from 'next-auth';
 
-import client from '@/common/mongoClient';
-
-if (
-  process.env.AZURE_AD_CLIENT_ID === undefined ||
-  process.env.AZURE_AD_CLIENT_SECRET === undefined ||
-  process.env.NEXTAUTH_SECRET === undefined
-) {
-  throw new Error('environment variables for auth not loaded!');
-}
-
-export const authOptions: NextAuthOptions = {
-  adapter: MongoDBAdapter(client.connect(), {
-    collections: {
-      Accounts: 'auth.accounts',
-      Sessions: 'auth.sessions',
-      Users: 'auth.users',
-      VerificationTokens: 'auth.verificationTokens',
-    },
-  }),
-  providers: [
-    AzureADProvider({
-      clientId: process.env.AZURE_AD_CLIENT_ID,
-      clientSecret: process.env.AZURE_AD_CLIENT_SECRET,
-      tenantId: process.env.AZURE_AD_TENANT_ID,
-    }),
-  ],
-};
+import authOptions from './authOptions';
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
